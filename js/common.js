46aac3eb04d8cb1a5f33cabf4384a020530bcc09
Vitaly Gol <vitalgolob@gmail.com>
2017-01-10 16:14:01 +0200

    google map

---
+++ js/common.js
@@ -0,0 +1,16 @@
+function initMap() {
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 48.464850, lng: 35.050297 },
+        scrollwheel: false,
+        zoom: 8
+    });
+}
+
+
+
+
+//AIzaSyCKJTk_adUHrZF7KN56enZE0ziN5PdJzzw
+//48.445747n, 35.056169e
+//48°27'53.5"N 35°03'01.1"E
+//48.464850, 35.050297
